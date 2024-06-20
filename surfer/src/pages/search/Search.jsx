cea8dea35613fe9a7fd04c7ae0da32ad81df5736
hongsoonho0723 <ghstns357@gmail.com>
2024-06-20 18:43:39 +0900

    통합검색 추가

--- surfer/src/pages/search/Search.jsx
+++ surfer/src/pages/search/Search.jsx
@@ -3,6 +3,9 @@ import "../../Search.css";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import SongItem from "../../components/SongItem";
+import AlbumItem from "../../components/AlbumItem";
+import LyricsItem from "../../components/LyricsItem";
+import PlaylistItem from "../../components/PlaylistItem";
 
 const Search = () => {
   const { keyword } = useParams();
@@ -13,6 +16,7 @@ const Search = () => {
     playlist: [],
     lyrics: [],
   });
+
   useEffect(() => {
     axios.get("http://localhost:8080/api/search/" + keyword).then((res) => {
       setSearchRes(res.data);
@@ -21,31 +25,121 @@ const Search = () => {
 
   return (
     <div className="Search">
-      <h3>{keyword} 검색 결과</h3>
+      <h2>'{keyword}' 검색 결과</h2>
 
-      {/* <div>
+      <div className="Song">
         <h3>노래</h3>
-        {SearchRes.map((search) => (
-          <SongItem key={search.song} searchkeyword={search} />
-        ))}
+        <div className="song-results">
+          <div className="song-result-section">
+            {SearchRes.song.filter((_, index) => index % 2 === 0).length > 0 ? (
+              SearchRes.song
+                .filter((_, index) => index % 2 === 0)
+                .map((song, index) => (
+                  <SongItem key={index} searchkeyword={song} />
+                ))
+            ) : (
+              <p>검색 결과가 없습니다</p>
+            )}
+          </div>
+          <div className="song-result-section">
+            {SearchRes.song.filter((_, index) => index % 2 !== 0).length > 0 ? (
+              SearchRes.song
+                .filter((_, index) => index % 2 !== 0)
+                .map((song, index) => (
+                  <SongItem key={index} searchkeyword={song} />
+                ))
+            ) : (
+              <p></p>
+            )}
+          </div>
+        </div>
+      </div>
+
+      {/* 앨범 */}
+
+      <div>
+        <h3>앨범</h3>
+
+        <div className="album-results">
+          {SearchRes.album.length > 0 ? (
+            SearchRes.album.map((album, index) => (
+              <AlbumItem key={index} searchkeyword={album} />
+            ))
+          ) : (
+            <p>검색 결과가 없습니다</p>
+          )}
+        </div>
+      </div>
+
+      {/* 플레이리스트 */}
+      <div>
+        <h3>플레이리스트</h3>
+
+        <div className="playlist-results">
+          {SearchRes.playlist.length > 0 ? (
+            SearchRes.playlist.map((playlist, index) => (
+              <PlaylistItem key={index} searchkeyword={playlist} />
+            ))
+          ) : (
+            <p>검색 결과가 없습니다</p>
+          )}
+        </div>
+      </div>
+
+      {/* 가사 */}
+
+      <div>
+        <h3>가사</h3>
+        <div className="lyrics-results">
+          {SearchRes.lyrics.length > 0 ? (
+            SearchRes.lyrics.map((lyrics, index) => (
+              <LyricsItem key={index} searchkeyword={lyrics} />
+            ))
+          ) : (
+            <p>검색 결과가 없습니다</p>
+          )}
+        </div>
+      </div>
+
+      {/*           
+          <h3>앨범</h3>
+          {SearchRes.album.length > 0 ? (
+            SearchRes.album.map((album, index) => (
+              <AlbumItem key={index} searchkeyword={album} />
+            ))
+          ) : (
+            <p>검색 결과가 없습니다</p>
+          )}
+
+          <h3>플레이리스트</h3>
+          {SearchRes.playlist.length > 0 ? (
+            SearchRes.playlist.map((playlist, index) => (
+              <PlaylistItem key={index} searchkeyword={playlist} />
+            ))
+          ) : (
+            <p>검색 결과가 없습니다</p>
+          )}
+
+          <h3>가사</h3>
+          {SearchRes.lyrics.length > 0 ? (
+            SearchRes.lyrics.map((lyrics, index) => (
+              <LyricsItem key={index} searchkeyword={lyrics} />
+            ))
+          ) : (
+            <p>검색 결과가 없습니다</p>
+          )}
+        </div>
+        
+        <div className="result-section">
+          {SearchRes.song.filter((_, index) => index % 2 !== 0).length > 0 ? (
+            SearchRes.song.filter((_, index) => index % 2 !== 0).map((song, index) => (
+              <SongItem key={index} searchkeyword={song} />
+            ))
+          ) : (
+            <p></p>
+          )}
+        </div>
       </div> */}
-
-      <h3>노래</h3>
-      <span>
-        {SearchRes.song.length > 0 ? (
-          SearchRes.song.map((song, index) => (
-            <SongItem key={index} searchkeyword={song} />
-          ))
-        ) : (
-          <p>No songs found</p>
-        )}
-      </span>
-
-      <h3>앨범</h3>
-
-      <h3>플레이리스트</h3>
-
-      <h3>가사</h3>
     </div>
   );
 };
